abcd4be4395cbce8ef20d90acaee6c2ddb982c6b
PRASUN-SITAULA <psitaula209@gmail.com>
2022-10-14 12:05:46 +0545

    Added Styling

--- base/models.py
+++ base/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AddFood(models.Model):
-    name_of_food = models.CharField(max_length=200)
+    name_of_food = models.CharField(max_length=150)
     calorie = models.IntegerField()
     protein = models.DecimalField(default=0,max_digits=20,decimal_places=4)
     fats = models.DecimalField(default=0,max_digits=20,decimal_places=4)
