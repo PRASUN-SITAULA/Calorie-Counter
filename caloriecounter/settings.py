2e6335911296ce11799d858df31e85b31157e3f0
PRASUN-SITAULA <psitaula209@gmail.com>
2022-10-17 22:03:21 +0545

    Added Background image

--- caloriecounter/settings.py
+++ caloriecounter/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-MEDIA_URL = ['base/images/']
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/ 'base/static/']
+STATICFILES_DIRS = [BASE_DIR/ 'base/static']
 # MEDIA_ROOT = BASE_DIR/ 'base/static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+            
